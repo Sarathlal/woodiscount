a3ece1f314cb13be0077de38a3db61e40b7405ac
sarath <sarath@sarath.system.com>
2018-03-07 15:12:04 +0530

    Enable option to select product from backend

--- README.md
+++ README.md
@@ -5,18 +5,17 @@
 1. Download plugin from [here](https://github.com/Sarathlal/woodiscount/archive/master.zip).
 2. Upload to Plugin folder
 3. Activate plugin
-4. Go to settings >> Discount Settings
-5. Add Product Ids in coma seperated (1, 2, 3, 4 etc)
-6. Add the amount for price check
+4. Go to Settings >> Discount Settings
+5. Select products that to be checked in cart
+6. Add the amount for price check. If this amount is greater than selected product price in cart, a discount will be applied with the given amount.
 7. Add the discount amount
 8. Save settings & You are done.
 
 ## Changelog
 
-1. 1.0 - only 1 product id allowed.
+1. 1.0 - Only 1 product id allowed.
 2. 1.1 - Coma seperated products ID allowed
+3. 1.2 - Option to select products enabled
 
 
-## Update
 
-1. Select product from back end
--- woodiscount.php
+++ woodiscount.php
@@ -4,18 +4,25 @@
      Plugin URI: https://sarathlal.com/
      Description: Allow discount as per cart items
      Author: Sarathlal N
-     Version: 1.0
+     Version: 1.2
      Author URI: https://sarathlal.com/
     */
    
    class WooDiscount {
    
        public function __construct() {
+		   add_action('admin_enqueue_scripts', array($this, 'enqueue_script'));
            add_action('admin_init', array($this, 'discount_setting'));
            add_action('admin_menu', array($this, 'create_options_page'));
            add_action( 'woocommerce_cart_calculate_fees', array($this, 'apply_discount'));
            
        }
+       
+		//Enque script
+		function enqueue_script() {
+			wp_enqueue_script('discount_script', plugin_dir_url(__FILE__) . 'js/select2.js');
+			wp_enqueue_style('discount_css', plugin_dir_url(__FILE__) . 'css/select2.min.css', false, '1.0.0');
+		}       
    
        //Register settings for discount with in new group - woo_discount_group
        function discount_setting() {
@@ -34,11 +41,37 @@
            ?>
 		<div>
 		   <h2>Add discount for WooCommerce products</h2>
+		   
+		   
+		   
+			<?php $selected_products = get_option('woo_products'); ?>
+			
 		   <form id="submit-data" method="post" action="options.php">
 			  <?php settings_fields('woo_discount_group'); ?>
 			  <p>
-				 <label for="woo_products">Enter Product Id to allow discount</label><br>
-				 <input type="text" id="woo_products" name="woo_products" value="<?php echo get_option('woo_products'); ?>" />
+				 <label for="woo_products">Select Products</label><br>
+				<?php  
+				$query = array(
+						'post_type'      => 'product',
+						'posts_per_page' => -1,
+					);
+				$queryObject = new WP_Query($query);
+				if ($queryObject->have_posts()) {
+					echo "<select  class='js-example-basic-single' id='woo_products' name='woo_products[]' multiple>";
+					while ($queryObject->have_posts()) {
+						$queryObject->the_post();
+						global $product;
+						$item_id = get_the_id();
+						if(in_array($item_id, $selected_products)){
+							$selected = "selected";
+						} else {
+							$selected = "";
+						}
+						echo "<option value='".get_the_id()."' ".$selected.">".get_the_title()."</option>";
+					}
+					echo "</select>";
+				}    
+				?>
 			  </p>
 			  <p>
 				 <label for="woo_price">Check Price</label><br>
@@ -58,9 +91,7 @@
 		function apply_discount() {
 			global $woocommerce;
 			$cartitems = $woocommerce->cart->get_cart();
-			$discountable_product = get_option('woo_products');	
-			$discountable_product_array = explode(',', $discountable_product);
-			$discountable_product_array = array_map('trim', $discountable_product_array);
+			$discountable_product_array = get_option('woo_products');
 			$price_for_discount = get_option('woo_price');
 			$discount_amount = get_option('woo_discount');
 			$discount_amount = -1 * abs($discount_amount);
